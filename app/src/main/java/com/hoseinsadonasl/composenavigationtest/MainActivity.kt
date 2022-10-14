d96e8aca10aebcafb300656bf8932bcaafe6db38
HoseinSadonasl <hoseinsadonasl@gmail.com>
2022-10-14 21:15:38 +0330

    Revert "secound comit"
    This reverts commit a2f6518c86e984b97ea7a992ec73c7a57d07c0da.

--- app/src/main/java/com/hoseinsadonasl/composenavigationtest/MainActivity.kt
+++ app/src/main/java/com/hoseinsadonasl/composenavigationtest/MainActivity.kt
@@ -6,7 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.hoseinsadonasl.composenavigationtest.ui.theme.ComposeNavigationTestTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,12 +22,22 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    //
-                    //
-                    //git a
+                    Greeting("Android")
                 }
             }
         }
     }
 }
 
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeNavigationTestTheme {
+        Greeting("Android")
+    }
+}

a2f6518c86e984b97ea7a992ec73c7a57d07c0da
HoseinSadonasl <hoseinsadonasl@gmail.com>
2022-10-14 19:30:24 +0330

    secound comit

--- app/src/main/java/com/hoseinsadonasl/composenavigationtest/MainActivity.kt
+++ app/src/main/java/com/hoseinsadonasl/composenavigationtest/MainActivity.kt
@@ -6,10 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.hoseinsadonasl.composenavigationtest.ui.theme.ComposeNavigationTestTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +19,12 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    //
+                    //
+                    //git a
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeNavigationTestTheme {
-        Greeting("Android")
-    }
-}
